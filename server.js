237ce501ebcd08530563a9d124adda7f87cd6329
ShazWarr <s13moha@siena.edu>
2024-04-29 20:50:31 -0400

    Added delete functionality

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(cors({
 
 }));
 
+
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database("data/lab9DB.db", (err) => {
     if (err) {
@@ -19,12 +20,15 @@ const db = new sqlite3.Database("data/lab9DB.db", (err) => {
     console.log('Connected to the SQlite database.');
 });
 
+
+
 // Middleware to parse JSON and URL-encoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // GET endpoint/handler for "/finance"
 app.get('/finance', (req, res) => {
+  
   db.all("SELECT * FROM finance", [], (err, rows) => {
     if (err) {
         res.status(500).send('Failed to retrieve data from the database.');
@@ -37,6 +41,7 @@ app.get('/finance', (req, res) => {
 
 //POST endpoint/Hanlder for "/finance"
 app.post('/finance', (req, res) => {
+  
   const { title, category, amount, type, date } = req.body;
   db.run(`INSERT INTO finance (title, category, amount, type, date) VALUES (?, ?, ?, ?, ?)`, [title, category, amount, type, date], (err) => {
       if (err) {
@@ -48,7 +53,36 @@ app.post('/finance', (req, res) => {
   });
 });
 
+//DELETE endpoint/Hanlder for "/finance"
+app.delete("/expenses/:id", async function (req, res) {
+  
+  const id = req.params.id;
+  const deleteQuery = "DELETE FROM finance WHERE id = ?";
+  try {
+    const delOut = await db.run(deleteQuery, id);
+    res.status(200).send("Expense deleted successfully");
+  } catch (error) {
+    res.status(404).send("Expense id not found");
+  }
+});
 
+//EDIT endpoint
+app.put("/expenses/:id", (req, res) => {
+  const { id } = req.params;
+  const { date, description, amount } = req.body;
+  const updateQuery = `UPDATE expenses SET date = ?, description = ?, amount = ? WHERE id = ?`;
+
+  // Same code as delete
+  db.run(updateQuery, [date, description, amount, id], (err)=> {
+    if(err){
+      res.status(500).send("Error in updating expense")
+    }
+    else{
+      res.status(200).send("Succesfully updated record")
+    }
+    
+  });
+});
 
 
 
